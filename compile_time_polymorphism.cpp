3403045d75e489689479306a19e1db1858df8504
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2022-07-05 14:56:50 +0530

    Add files via upload

---
+++ compile_time_polymorphism.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Vehicle{
+	public:
+	Vehicle(){
+		cout<<"vehicle constructor call"<<endl;
+	}
+	void print(){
+		cout<<"Vehicle print function"<<endl;
+	}
+};
+class Car: public Vehicle{
+	public:
+	Car(){
+		cout<<"car constructor call"<<endl;
+	}
+	void print(){
+		cout<<"car print function call"<<endl;
+	}
+};
+int main(){
+	Vehicle v;
+	Car c;
+	v.print();
+	c.print();
+	Vehicle *v1 = new Vehicle;
+	Vehicle *v2;
+	//v2 = &v;
+	v2 = &c;
+	v1->print();
+	v2->print(); // compile time decision print vehicle but actually points to Car
+}
